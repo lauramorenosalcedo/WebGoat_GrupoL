0950cf6abd86a3b9ed943a362d7d515a6702b8a3
nachoalv <ignacioalvaneznunez@gmail.com>
2024-04-05 15:35:00 +0200

    Update Assignment5.java

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
@@ -55,7 +55,7 @@ public class Assignment5 extends AssignmentEndpoint {
       return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
     }
     try (var connection = dataSource.getConnection()) {
-      String query = "select password from challenge_users where userid = ? and password = ?"
+      String query = "select password from challenge_users where userid = ? and password = ?";
       try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, username_login);
             statement.setString(2, password_login);
